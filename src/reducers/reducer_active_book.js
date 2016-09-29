2b17d9adb8156e4704cd8db69a0f7080203ae7ac
Jennifer <jmshakala@gmail.com>
2016-09-28 23:11:59 -0500

    created active book reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,12 @@
+// reducers have two arguments: current state and action
+// reducers only called with action occurs
+// state is state reducer is responsible for
+export default function(state = null, action) {
+  switch(action.type) {
+  	case 'BOOK_SELECTED':
+  		return action.payload
+  }	
+
+  return state
+}
+
